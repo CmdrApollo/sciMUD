c38c09855abcfacc014b913a3d0e1c6493cf811e
Mikeymoo5 <moodids@outlook.com>
2025-04-27 15:52:32 -0500

    Added weapon item class, and begin work on creating items from json file

--- src/items.py
+++ src/items.py
@@ -1,5 +1,6 @@
 class Item:
     def __init__(self, name: str, description: str, needs_recipient: bool = False) -> None:
+        #TODO: add weight
         self.name: str = name
         self.description: str = description
         self.needs_recipient: bool = needs_recipient
@@ -17,24 +18,33 @@ class Item:
 
     def use(self, recipient: str, player, world) -> str:
         return ''
+class Weapon(Item):
+    def __init__(self, name: str,verb: str, description: str, stats: dict[str,float]):
+        super().__init__(name, description, True)
+        self.verb = verb
+        self.stats = stats
 
-class Knife(Item):
-    def __init__(self):
-        super().__init__("Knife", "A small knife.", True)
-    
     def use(self, recipient: str, player, world) -> str:
         if self.try_use(recipient, player, world):
             if recipient in world.state.get_room(player.current_room).entities:
                 world.state.get_room(player.current_room).entities.remove(recipient)
                 world.send_message_to_players_in_room(player, f"Player '{player.name}' stabs the {recipient}.", player.current_room)
-                return f"You viciously stab the {recipient}."
+                return f"You viciously {self.verb} the {recipient}."
             else:
                 return f"There is no {recipient} here."
         return self.fail_message
 
+class Knife(Weapon):
+    def __init__(self):
+        super().__init__("Knife", "poke", "A small knife.", {
+            'strength': 10,
+            'attack': 2,
+        })
+    
 def get_item_with_name(name: str) -> Item:
     match name:
         case "knife":
             return Knife
         case _:
             return None
+        
